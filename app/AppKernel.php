36571344cfacd3ead0ef9be0213ed36c13004104
qmdeveloppement <qmdeveloppement@gmail.com>
2014-06-17 14:24:15 +0200

    ajout des extensions gedmo/doctrine + bundle stof

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Novactive\InterfaceBundle\NovactiveInterfaceBundle(),
             new Novactive\AdminBundle\NovactiveAdminBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
