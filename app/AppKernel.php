92ca81d554757429e246d990256677a4eae12310
qmdeveloppement <qmdeveloppement@gmail.com>
2014-06-17 16:28:51 +0200

    ajout du bundle BraincraftedBootstrapBundle (+ lessphp)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Novactive\InterfaceBundle\NovactiveInterfaceBundle(),
             new Novactive\AdminBundle\NovactiveAdminBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
