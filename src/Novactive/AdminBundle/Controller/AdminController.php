b40b018e39f878edfcc976da7e5b511a4be09a58
q.morlais <q.morlais@WKTUNISDEV03D.novactive.local>
2014-06-24 10:49:55 +0200

    admin controller baseroute

--- src/Novactive/AdminBundle/Controller/AdminController.php
+++ src/Novactive/AdminBundle/Controller/AdminController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/admin")
+ * @Route("/")
  */
 class AdminController extends Controller
 {
@@ -26,31 +26,6 @@ class AdminController extends Controller
      */
     public function soapAction(Request $request)
     {
-//        echo "<pre>";
-//        $this->get('novactive_admin.importer.tour_planning')->execute();
-////        $this->get('novactive_admin.importer.parcel_details')->execute();
-//        echo "</pre>";die;
-//        $wsParams=$this->container->getParameter('czn_webservice');
-//        $client=new \SoapClient($wsParams['wsdl_location']);
-//        
-//        $wsReq=$wsParams;
-//        unset($wsReq['wsdl_location']);
-//        $wsReq+=array(
-//            'customer_center'=>'750',
-//            'shipmentnumber'=>"250750002582962"
-//        );
-        echo "<pre>";
-//        var_dump($client->__getFunctions());
-//        var_dump($client->__getTypes());
-        foreach (array('250750002545289','2507502583059')as $ssp){
-//            foreach (array($ssp,substr($ssp,0,-3),substr($ssp,0,-4),substr($ssp,0,-5)) as $sp){
-            echo ("env : ".$ssp. " <br/>");
-            echo ("ret : ".var_dump($this->get('webservice')->getShipmentTrace(750,$ssp)). " <br/>");
-//            }
-            echo ('---------<br/>');
-        }
-        echo "</pre>";
-        die;
-//        return array();
+
     }
 }
