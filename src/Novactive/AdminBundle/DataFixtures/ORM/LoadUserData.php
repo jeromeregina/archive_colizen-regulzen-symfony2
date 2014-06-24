26a4418f828b95f6299212210eeb8e45479ad987
Fayez Naccache <fnaccache@novactive.com>
2014-06-24 14:32:44 +0100

    add user fixtures

---
+++ src/Novactive/AdminBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Novactive\AdminBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\Doctrine;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Novactive\UserBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@novactive.com')
+            ->setUsername('admin')
+            ->setPlainPassword('admin')
+            ->setEnabled(true)
+            ->setSuperAdmin(true);
+
+        $manager->persist($user);
+
+
+        $user = new User();
+        $user->setEmail('regul@novactive.com')
+            ->setUsername('regul')
+            ->setPlainPassword('regul')
+            ->setEnabled(true)
+            ->setRoles(array('ROLE_REGUL'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
