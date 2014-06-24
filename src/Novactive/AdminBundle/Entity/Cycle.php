26aadfb19a755f85ee002d09e694803bc6ec60d6
q.morlais <q.morlais@WKTUNISDEV03D.novactive.local>
2014-06-24 10:45:18 +0200

    entity cycle fix, tour repo

--- src/Novactive/AdminBundle/Entity/Cycle.php
+++ src/Novactive/AdminBundle/Entity/Cycle.php
@@ -23,25 +23,11 @@ class Cycle
     private $id;
 
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="CYCLE_number", type="integer", nullable=false)
-     */
-    private $number;
-
-    /**
      * @var string
      *
      * @ORM\Column(name="CYCLE_name", type="string", length=45, nullable=false)
      */
     private $name;
-
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="CYCLE_is_excluded", type="boolean", nullable=false, options={"default"=true})
-     */
-    private $isExcluded = true;
 
     /**
     * @var datetime $created
@@ -70,29 +56,7 @@ class Cycle
         return $this->id;
     }
 
-    /**
-     * Set number
-     *
-     * @param integer $number
-     * @return Cycle
-     */
-    public function setNumber($number)
-    {
-        $this->number = $number;
-
-        return $this;
-    }
-
-    /**
-     * Get number
-     *
-     * @return integer 
-     */
-    public function getNumber()
-    {
-        return $this->number;
-    }
-
+   
     /**
      * Set name
      *
@@ -116,28 +80,7 @@ class Cycle
         return $this->name;
     }
 
-    /**
-     * Set isExcluded
-     *
-     * @param boolean $isExcluded
-     * @return Cycle
-     */
-    public function setIsExcluded($isExcluded)
-    {
-        $this->isExcluded = (boolean) $isExcluded;
-
-        return $this;
-    }
-
-    /**
-     * Get isExcluded
-     *
-     * @return boolean 
-     */
-    public function getIsExcluded()
-    {
-        return $this->isExcluded;
-    }
+    
       /**
      * Set created
      *
---
+++ src/Novactive/AdminBundle/Repository/Tour.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Novactive\AdminBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class Tour extends EntityRepository
+{
+}
