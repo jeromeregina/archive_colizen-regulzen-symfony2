ea578e3cea7218787a6af4fe67ed0ea670ffef58
q.morlais <q.morlais@WKTUNISDEV03D.novactive.local>
2014-06-25 16:13:46 +0200

    modifs fixtures cycle

--- src/Novactive/AdminBundle/DataFixtures/ORM/LoadCycleData.php
+++ src/Novactive/AdminBundle/DataFixtures/ORM/LoadCycleData.php
@@ -18,19 +18,25 @@ class LoadCycleData implements FixtureInterface
         $cycle = new Cycle();
         $cycle->setName('Matin')
             ->setStart(\DateTime::createFromFormat('H:i:s', '00:00:00'))
-            ->setEnd(\DateTime::createFromFormat('H:i:s', '12:59:00'));
+            ->setEnd(\DateTime::createFromFormat('H:i:s', '12:59:00'))
+            ->setTourCodeFormat('^(1|2|3)\d{2}$')
+                ;
         $manager->persist($cycle);
 
         $cycle = new Cycle();
         $cycle->setName('Après Midi')
             ->setStart(\DateTime::createFromFormat('H:i:s', '13:00:00'))
-            ->setEnd(\DateTime::createFromFormat('H:i:s', '21:59:00'));
+            ->setEnd(\DateTime::createFromFormat('H:i:s', '21:59:00'))
+            ->setTourCodeFormat('^(7)\d{2}$')
+                ;
         $manager->persist($cycle);
 
         $cycle = new Cycle();
         $cycle->setName('Soir')
             ->setStart(\DateTime::createFromFormat('H:i:s', '22:00:00'))
-            ->setEnd(\DateTime::createFromFormat('H:i:s', '23:59:00'));
+            ->setEnd(\DateTime::createFromFormat('H:i:s', '23:59:00'))
+            ->setTourCodeFormat('^(8)\d{2}$')
+                ;
         $manager->persist($cycle);
 
 
