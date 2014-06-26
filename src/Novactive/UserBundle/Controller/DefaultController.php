740452c004930704c9b294f1d8ef5169316590d9
Fayez Naccache <fnaccache@novactive.com>
2014-06-26 12:10:11 +0100

    format code

--- src/Novactive/UserBundle/Controller/DefaultController.php
+++ src/Novactive/UserBundle/Controller/DefaultController.php
@@ -3,17 +3,8 @@
 namespace Novactive\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+
 }
--- src/Novactive/UserBundle/Listener/Login.php
+++ src/Novactive/UserBundle/Listener/Login.php
@@ -2,37 +2,42 @@
 namespace Novactive\UserBundle\Listener;
 
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Doctrine\ORM\EntityManagerInterface; 
-use Doctrine\ORM\EntityManager; 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class Login {
+class Login
+{
     /**
      *
      * @var EntityManager
      */
     private $em;
+
     /**
      *
      * @var Session
      */
     private $session;
-    public function __construct(EntityManagerInterface $em, Session $session) {
+
+    public function __construct(EntityManagerInterface $em, Session $session)
+    {
         $this->em=$em;
         $this->session =$session;
     }
-    
-    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event){
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
        $user = $event->getAuthenticationToken()->getUser();
         if ($user instanceof UserInterface) {
-            $sessionRepo=$this->em->getRepository('NovactiveAdminBundle:Session');
+            $sessionRepo = $this->em->getRepository('NovactiveAdminBundle:Session');
 //            si l'on veu 'déloger' l'utilisateur déjà loggué
 //            $existingSessions=$sessionRepo->findByUser($user);
-//            foreach ($existingSessions as $es){
+//            foreach ($existingSessions as $es) {
 //                $this->em->remove($es);
 //            }
-            $sessionObject=$sessionRepo->findOneById($this->session->getId());
+            $sessionObject = $sessionRepo->findOneById($this->session->getId());
             $sessionObject->setUser($user);
             $this->em->flush();
         }
