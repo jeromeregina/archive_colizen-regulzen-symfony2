4b1193190d1406abc308b938b9210ea5be653d0e
Fayez Naccache <fnaccache@novactive.com>
2014-07-01 09:22:02 +0100

    Fix fixtures

--- src/Colizen/AdminBundle/DataFixtures/ORM/LoadSiteData.php
+++ src/Colizen/AdminBundle/DataFixtures/ORM/LoadSiteData.php
@@ -7,7 +7,7 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Colizen\UserBundle\Entity\User;
 
-class LoadUserData implements FixtureInterface
+class LoadSiteData implements FixtureInterface
 {
 
     /**
@@ -15,24 +15,6 @@ class LoadUserData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setEmail('admin@novactive.com')
-            ->setUsername('admin')
-            ->setPlainPassword('admin')
-            ->setEnabled(true)
-            ->setSuperAdmin(true);
-
-        $manager->persist($user);
-
-
-        $user = new User();
-        $user->setEmail('regul@novactive.com')
-            ->setUsername('regul')
-            ->setPlainPassword('regul')
-            ->setEnabled(true)
-            ->setRoles(array('ROLE_REGUL'));
-
-        $manager->persist($user);
 
         $manager->flush();
     }
