5065c34a61aff68f2c6d647e15d920a0e4f599b5
Fayez Naccache <fnaccache@novactive.com>
2014-07-01 08:58:59 +0100

    Intégration d'ace avec le BO

--- src/Colizen/AdminBundle/Menu/Builder.php
+++ src/Colizen/AdminBundle/Menu/Builder.php
@@ -5,19 +5,20 @@ namespace Colizen\AdminBundle\Menu;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
-class Builder extends ContainerAware{
-      public function adminMenu(FactoryInterface $factory, array $options)
+class Builder extends ContainerAware
+{
+    public function adminMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('admin');
 
-        $menu->addChild('Retour à l\'interface', array('route' => 'regulzen_interface_index'));
-        $menu->addChild('Logs imports');
+        //$menu->addChild('Retour à l\'interface', array('route' => 'regulzen_interface_index', 'attributes' => array('class' => 'purple')));
+        $menu->addChild('Logs imports', array('attributes' => array('class' => 'purple')));
         $menu['Logs imports']->addChild('Logs imports de fichiers', array('route' => 'admin_logs_imports_list'));
         $menu['Logs imports']->addChild('Logs imports webservices', array('route' => 'regulzen_interface_index'));
-        $menu->addChild('Créneaux', array('route' => 'regulzen_interface_index'));
-        $menu->addChild('Comptes chargeurs', array('route' => 'admin_sender_account_list'));
-        $menu->addChild('Sites', array('route' => 'admin_site_list'));
-        $menu->addChild('Cycles', array('route' => 'admin_cycle_list'));
+        $menu->addChild('Créneaux', array('route' => 'regulzen_interface_index', 'attributes' => array('class' => 'purple')));
+        $menu->addChild('Comptes chargeurs', array('route' => 'admin_sender_account_list', 'attributes' => array('class' => 'purple')));
+        $menu->addChild('Sites', array('route' => 'admin_site_list', 'attributes' => array('class' => 'purple')));
+        $menu->addChild('Cycles', array('route' => 'admin_cycle_list', 'attributes' => array('class' => 'purple')));
 
         return $menu;
     }
