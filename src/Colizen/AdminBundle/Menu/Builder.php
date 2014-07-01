6640bb5af3dcc87b0f30d2793556c3ed57495221
q.morlais <q.morlais@WKTUNISDEV03D.novactive.local>
2014-07-01 16:24:48 +0200

    crud status

--- src/Colizen/AdminBundle/Menu/Builder.php
+++ src/Colizen/AdminBundle/Menu/Builder.php
@@ -16,6 +16,7 @@ class Builder extends ContainerAware
         $menu['Logs imports']->addChild('Logs imports de fichiers', array('route' => 'admin_logs_imports_list'));
         $menu['Logs imports']->addChild('Logs imports webservices', array('route' => 'regulzen_interface_index'));
         $menu->addChild('Codes Tournées', array('route' => 'admin_tour_code_list', 'attributes' => array('class' => 'purple')));
+        $menu->addChild('Status', array('route' => 'admin_status_list', 'attributes' => array('class' => 'purple')));
         $menu->addChild('Comptes chargeurs', array('route' => 'admin_shipper_account_list', 'attributes' => array('class' => 'purple')));
         $menu->addChild('Sites', array('route' => 'admin_site_list', 'attributes' => array('class' => 'purple')));
         $menu->addChild('Cycles', array('route' => 'admin_cycle_list', 'attributes' => array('class' => 'purple')));
