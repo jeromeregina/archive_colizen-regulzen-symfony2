1e421b142279c2ba0c9b69994512cc5c384da2d7
q.morlais <q.morlais@WKTUNISDEV03D.novactive.local>
2014-07-01 14:12:16 +0200

    fixtures site

--- src/Colizen/AdminBundle/DataFixtures/ORM/LoadSiteData.php
+++ src/Colizen/AdminBundle/DataFixtures/ORM/LoadSiteData.php
@@ -5,7 +5,7 @@ namespace Colizen\AdminBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\Doctrine;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Colizen\UserBundle\Entity\User;
+use Colizen\AdminBundle\Entity\Site;
 
 class LoadSiteData implements FixtureInterface
 {
@@ -15,7 +15,38 @@ class LoadSiteData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-
+        $site = new Site();
+        $site->setName('Paris')
+             ->setCodeColizen('K18')
+             ->setCodeImtech('CAP18')
+             ->setIsActive(true)
+             ->setNumber(750);
+        $manager->persist($site);
+        
+        $site = new Site();
+        $site->setName('Lyon')
+             ->setCodeColizen('LYS')
+             ->setCodeImtech('LYS')
+             ->setIsActive(true)
+             ->setNumber(690);
+        $manager->persist($site);
+        
+        $site = new Site();
+        $site->setName('Lille')
+             ->setCodeColizen('LIL')
+             ->setCodeImtech('LIL')
+             ->setIsActive(true)
+             ->setNumber(590);
+        $manager->persist($site);
+        
+        $site = new Site();
+        $site->setName('Marseille')
+             ->setCodeColizen('OMV')
+             ->setCodeImtech('OMV')
+             ->setIsActive(true)
+             ->setNumber(130);
+        $manager->persist($site);
+        
         $manager->flush();
     }
 }
