518c59a18e53918d965d913aa310655649d51fce
Fayez Naccache <fnaccache@novactive.com>
2014-07-01 09:22:19 +0100

    Fix Menu

--- src/Colizen/AdminBundle/Menu/Builder.php
+++ src/Colizen/AdminBundle/Menu/Builder.php
@@ -16,7 +16,7 @@ class Builder extends ContainerAware
         $menu['Logs imports']->addChild('Logs imports de fichiers', array('route' => 'admin_logs_imports_list'));
         $menu['Logs imports']->addChild('Logs imports webservices', array('route' => 'regulzen_interface_index'));
         $menu->addChild('Créneaux', array('route' => 'regulzen_interface_index', 'attributes' => array('class' => 'purple')));
-        $menu->addChild('Comptes chargeurs', array('route' => 'admin_sender_account_list', 'attributes' => array('class' => 'purple')));
+        $menu->addChild('Comptes chargeurs', array('route' => 'admin_shipper_account_list', 'attributes' => array('class' => 'purple')));
         $menu->addChild('Sites', array('route' => 'admin_site_list', 'attributes' => array('class' => 'purple')));
         $menu->addChild('Cycles', array('route' => 'admin_cycle_list', 'attributes' => array('class' => 'purple')));
 
