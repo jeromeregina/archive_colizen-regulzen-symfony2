fd220ae39bb8ed12fb7250d07c89fc4535bfa75c
q.morlais <q.morlais@WKTUNISDEV03D.novactive.local>
2014-07-03 11:56:44 +0200

    ajout import de données webservice (base de travail)

--- src/Colizen/AdminBundle/Repository/Status.php
+++ src/Colizen/AdminBundle/Repository/Status.php
@@ -4,8 +4,22 @@ namespace Colizen\AdminBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
 class Status extends EntityRepository{
+    /**
+     * 
+     * @return \Doctrine\ORM\Query
+     */
     public function findAllQuery(){
+        
         $qb=$this->createQueryBuilder('s');
+        
         return $qb->select('s')->getQuery();
+    }
+    /**
+     * récupère le status de type "PLANIF"
+     * 
+     * @return \Colizen\AdminBundle\Entity\Status
+     */
+    public function getPlanif(){
+        return $this->find(-1);
     }
 }
